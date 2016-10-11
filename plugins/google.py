784a31744dbb30b42475561615a6947f2c21b723
Denis K <denis.kolosov@gmail.com>
2016-10-11 23:21:50 +0300

    Add options parsing

--- plugins/google.py
+++ plugins/google.py
@@ -1,3 +1,3 @@
 
-def run_me():
-    print('hello')
+def print_me(source, to, string_to_translate=""):
+    print("from %s to %s — %s" % (source, to, string_to_translate))
--- translate.py
+++ translate.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 from functools import partial
 
@@ -14,6 +15,13 @@ plugin_source = plugin_base.make_plugin_source(searchpath=[get_path('./plugins')
 with plugin_source:
     from translate.plugins import google
 
+parser = argparse.ArgumentParser(description="Translator app with plugins.")
+
+parser.add_argument("string_to_translate", help="Some text to translate (don't forget to quote it)")
+parser.add_argument("--source", help="Language to translate from")
+parser.add_argument("--to", help="Language to translate to")
+
 
 if __name__ == "__main__":
-    google.run_me()
+    args = parser.parse_args()
+    google.print_me(args.source, args.to, args.string_to_translate)
