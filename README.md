c2d7044a4bab0b51c0d9305e9ee02ef2d8d1c9ff
Denis K <denis.kolosov@gmail.com>
2016-10-12 16:44:33 +0300

    Add requirements.txt, refactor main function

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # pytrans
 Cli translator with plugins built using python
+
+#### Installation:
+``` bash
+$ git clone https://github.com/thefivekey/pytrans.git
+$ cd pytrans/
+$ pip install -r requirements.txt
+```
+
+#### Usage:
+``` bash
+$ python translate.py --from de --to ru 'hallo welt'
+Google:  Привет мир
+```
+
+default plugin is first in 'plugins' directory. You can also specify 
+a plugin, languages to translate, etc. More info is available by -h
+command:
+``` bash
+$ python translate.py -h
+usage: translate.py [-h] [--verbose] [--from TRANSLATE_FROM]
+                    [--to TRANSLATE_TO]
+                    text_to_translate [{google,offline,yandex}]
+
+Translator app with plugins.
+
+positional arguments:
+  text_to_translate     Some text to translate (don't forget to quote it)
+  {google,offline,yandex}
+                        Name of translation plugin
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --verbose             Displays results from all plugins.
+  --from TRANSLATE_FROM
+                        Language to translate from
+  --to TRANSLATE_TO     Language to translate to
+```
+
+
+#### Plugins:
+To create a plugin, simply create a python file inside 'plugins' folder
+with 'translate' function which has following signature: 
+``` python
+translate(translate_from, translate_to, string_to_translate="")
+```
+It should print results of translation to stdout.
--- translate.py
+++ translate.py
@@ -8,27 +8,11 @@ import sys
 from pluginbase import PluginBase
 
 
-# For easier usage calculate the path relative to here.
-here = os.path.abspath(os.path.dirname(__file__))
-get_path = partial(os.path.join, here)
-
-plugin_base = PluginBase(package='translate.plugins')
-plugin_source = plugin_base.make_plugin_source(searchpath=[get_path('./plugins')])
-plugin_list = plugin_source.list_plugins()
-
-parser = argparse.ArgumentParser(description="Translator app with plugins.")
-
-parser.add_argument("--verbose", help="Displays results from all plugins.", action="store_true")
-parser.add_argument("--from", dest="translate_from", help="Language to translate from", default='en')
-parser.add_argument("--to", dest="translate_to", help="Language to translate to", default='ru')
-parser.add_argument("text_to_translate", help="Some text to translate (don't forget to quote it)")
-parser.add_argument("plugin_name", help="Name of translation plugin", choices=plugin_list, nargs='?', type=str)
-
-
-def load_and_validate_plugin(plugin_name):
+def _load_and_validate_plugin(plugin_name, plugin_source):
     """
     Validate and load plug from plugin source.
     :param plugin_name: name of plugin to load
+    :param plugin_source: plugin source object
     :return: valid plugin object or None
     """
     plugin = plugin_source.load_plugin(plugin_name)
@@ -42,7 +26,23 @@ def load_and_validate_plugin(plugin_name):
     return plugin
 
 
-if __name__ == "__main__":
+def run():
+    parser = argparse.ArgumentParser(description="Translator app with plugins.")
+
+    # For easier usage calculate the path relative to here.
+    here = os.path.abspath(os.path.dirname(__file__))
+    get_path = partial(os.path.join, here)
+
+    plugin_base = PluginBase(package='translate.plugins')
+    plugin_source = plugin_base.make_plugin_source(searchpath=[get_path('./plugins')])
+    plugin_list = plugin_source.list_plugins()
+
+    parser.add_argument("--verbose", help="Displays results from all plugins.", action="store_true")
+    parser.add_argument("--from", dest="translate_from", help="Language to translate from", default='en')
+    parser.add_argument("--to", dest="translate_to", help="Language to translate to", default='ru')
+    parser.add_argument("text_to_translate", help="Some text to translate (don't forget to quote it)")
+    parser.add_argument("plugin_name", help="Name of translation plugin", choices=plugin_list, nargs='?', type=str)
+
     args = parser.parse_args()
 
     thread_list = []
@@ -54,13 +54,13 @@ if __name__ == "__main__":
     plugin_name = args.plugin_name if args.plugin_name else plugin_list[0]
 
     if plugin_name and not args.verbose:
-        plugin = load_and_validate_plugin(plugin_name)
+        plugin = _load_and_validate_plugin(plugin_name, plugin_source)
         if plugin:
             plugin.translate(args.translate_from, args.translate_to, args.text_to_translate)
 
     else:
         for name in plugin_list:
-            plugin = load_and_validate_plugin(name)
+            plugin = _load_and_validate_plugin(name, plugin_source)
 
             if not plugin:
                 continue
@@ -71,3 +71,7 @@ if __name__ == "__main__":
 
         for t in thread_list:
             t.join()
+
+
+if __name__ == "__main__":
+    run()
